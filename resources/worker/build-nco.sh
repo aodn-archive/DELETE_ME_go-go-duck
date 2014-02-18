c20d9c2f99e1c86ae7ae0acd84003f29dd25ebba
Dan Fruehauf <malkodan@gmail.com>
2014-02-18 15:10:32 +1100

    cleanup in build-nco.sh script

--- resources/worker/build-nco.sh
+++ resources/worker/build-nco.sh
@@ -9,6 +9,9 @@ main() {
     nco_src_basename=`basename $NCO_SRC`
     debian_src_basename=`basename $DEBIAN_CONTROL_SRC`
 
+    rm -rf nco-$NCO_VERSION
+    rm -f $nco_src_basename $debian_src_basename
+
     # retrieve
     wget $NCO_SRC
     wget $DEBIAN_CONTROL_SRC
