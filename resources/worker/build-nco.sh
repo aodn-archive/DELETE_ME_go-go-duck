2c896cde3d0bd731d165f25d86356cd92b2a764c
Dan Fruehauf <malkodan@gmail.com>
2014-02-18 15:08:28 +1100

    script to build recent nco utils

---
+++ resources/worker/build-nco.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -r NCO_VERSION=4.3.4
+declare -r NCO_SRC=http://dust.ess.uci.edu/nco/src/nco_${NCO_VERSION}.orig.tar.gz
+declare -r DEBIAN_CONTROL_SRC=http://dust.ess.uci.edu/nco/src/nco_${NCO_VERSION}-1.debian.tar.gz
+
+# main
+main() {
+    nco_src_basename=`basename $NCO_SRC`
+    debian_src_basename=`basename $DEBIAN_CONTROL_SRC`
+
+    # retrieve
+    wget $NCO_SRC
+    wget $DEBIAN_CONTROL_SRC
+
+    # extract
+    tar -xf $nco_src_basename && \
+        cd nco-$NCO_VERSION && \
+        tar -xf ../$debian_src_basename
+
+    # build!
+    dpkg-buildpackage -B -nc
+}
+
+main "$@"
