5b3d99bd73e4be87e6ec01a58f5387f45b43d875
Jon Burgess <jkburges@gmail.com>
2014-02-25 15:42:39 +1100

    Introduced service dedicated to notifications, since it'll be needed in a couple of different spots.

--- grails-app/services/au/org/emii/gogoduck/job/JobExecutorService.groovy
+++ grails-app/services/au/org/emii/gogoduck/job/JobExecutorService.groovy
@@ -1,45 +1,19 @@
 package au.org.emii.gogoduck.job
 
-import org.springframework.context.i18n.LocaleContextHolder
 import au.org.emii.gogoduck.worker.Worker
 
 class JobExecutorService {
 
     def grailsApplication
     def jobStoreService
-    def mailService
-    def messageSource
+    def notificationService
 
     def run(job) {
-        sendJobRegisteredNotification(job)
+        notificationService.sendJobRegisteredNotification(job)
         jobStoreService.makeDir(job)
         jobStoreService.writeToFileAsJson(job)
         getWorker(job).run()
-        sendJobSuccessNotification(job)
-    }
-
-    def sendJobRegisteredNotification(job) {
-        mailService.sendMail {
-            to job.emailAddress.toString()
-            subject getRegisteredNotificationSubject(job)
-            body getRegisteredNotificationBody(job)
-        }
-    }
-
-    def getRegisteredNotificationSubject(job) {
-        messageSource.getMessage(
-            'job.registered.subject',
-            [job.uuid].toArray(),
-            LocaleContextHolder.locale
-        )
-    }
-
-    def getRegisteredNotificationBody(job) {
-        messageSource.getMessage(
-            'job.registered.body',
-            [job.uuid].toArray(),
-            LocaleContextHolder.locale
-        )
+        notificationService.sendJobSuccessNotification(job)
     }
 
     def getWorker(job) {
@@ -48,30 +22,6 @@ class JobExecutorService {
             job: job,
             outputFilename: jobStoreService.getAggrPath(job),
             fileLimit: grailsApplication.config.worker.fileLimit
-        )
-    }
-
-    def sendJobSuccessNotification(job) {
-        mailService.sendMail {
-            to job.emailAddress.toString()
-            subject getSuccessNotificationSubject(job)
-            body getSuccessNotificationBody(job)
-        }
-    }
-
-    def getSuccessNotificationSubject(job) {
-        messageSource.getMessage(
-            'job.success.subject',
-            [job.uuid].toArray(),
-            LocaleContextHolder.locale
-        )
-    }
-
-    def getSuccessNotificationBody(job) {
-        messageSource.getMessage(
-            'job.success.body',
-            [job.uuid, job.aggrUrl].toArray(),
-            LocaleContextHolder.locale
         )
     }
 }
