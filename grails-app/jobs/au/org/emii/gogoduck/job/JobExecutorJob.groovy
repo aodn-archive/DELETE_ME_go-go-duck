7fec44ef843854830f266b5c3f01390cadd6f3d8
Jon Burgess <jkburges@gmail.com>
2014-02-25 16:12:09 +1100

    Introduced quartz job, in order to implement "one job at a time" policy.

---
+++ grails-app/jobs/au/org/emii/gogoduck/job/JobExecutorJob.groovy
@@ -0,0 +1,29 @@
+package au.org.emii.gogoduck.job
+
+import au.org.emii.gogoduck.worker.Worker
+
+class JobExecutorJob {
+
+    def sessionRequired = false
+    def concurrent = false
+
+    def grailsApplication
+    def jobStoreService
+    def notificationService
+
+    def run(job) {
+        jobStoreService.makeDir(job)
+        jobStoreService.writeToFileAsJson(job)
+        getWorker(job).run()
+        notificationService.sendJobSuccessNotification(job)
+    }
+
+    def getWorker(job) {
+        return new Worker(
+            shellCmd: grailsApplication.config.worker.cmd,
+            job: job,
+            outputFilename: jobStoreService.getAggrPath(job),
+            fileLimit: grailsApplication.config.worker.fileLimit
+        )
+    }
+}
