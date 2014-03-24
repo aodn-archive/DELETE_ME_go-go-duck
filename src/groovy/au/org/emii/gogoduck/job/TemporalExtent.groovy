773bbb95720e89ad42cc06b2fc3716a3b01a2f55
dnahodil <dnahodil@utas.edu.au>
2014-03-24 14:24:13 +1100

    Incoming date format validation in handled by its own class

--- src/groovy/au/org/emii/gogoduck/job/TemporalExtent.groovy
+++ src/groovy/au/org/emii/gogoduck/job/TemporalExtent.groovy
@@ -1,29 +1,12 @@
 package au.org.emii.gogoduck.job
 
-import org.joda.time.format.DateTimeFormat
-import org.joda.time.format.DateTimeFormatter
-
 @grails.validation.Validateable
 class TemporalExtent {
     String start
     String end
 
     static constraints = {
-        start validator: dateFormatValidator
-        end   validator: dateFormatValidator
-    }
-
-    static def dateFormatValidator = {
-
-        try {
-            DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-            fmt.parseDateTime(it)
-
-            return true
-        }
-        catch (Exception e) {
-
-            return false
-        }
+        start validator: DateFormatValidator.isValid
+        end   validator: DateFormatValidator.isValid
     }
 }
